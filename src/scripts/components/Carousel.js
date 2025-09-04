a473aae07ffd4b7984cfa49cf27f48d8333be5a0
adamelalfy <143737572+adamelalfy@users.noreply.github.com>
2025-09-04 11:50:41 -0400

    swiper termine

--- src/scripts/components/Carousel.js
+++ src/scripts/components/Carousel.js
@@ -5,10 +5,11 @@ export default class Carousel {
     this.element = element;
     this.options = {
       slidesPerView: 1,
-      spaceBetween: 20,
+      spaceBetween: 0,
       pagination: {
         el: this.element.querySelector('.swiper-pagination'),
         type: 'bullets',
+        clickable: true,
       },
       navigation: {
         nextEl: this.element.querySelector('.swiper-button-next'),
@@ -20,35 +21,9 @@ export default class Carousel {
   }
 
   setOptions() {
-    // Pour Swipers qui split pour viewport plus large que 768px
-    if ('split' in this.element.dataset) {
-      this.options.breakpoints = {
-        768: {
-          slidesPerView: 2.5,
-        },
-      };
-    }
-
-    // Pour autoplay Swipers
-    if ('autoplay' in this.element.dataset) {
-      this.options.autoplay = false;
-      this.options.autoplay = {
-        delay: 0.01,
-        pauseOnMouseEnter: true,
-        disableOnInteraction: false,
-      };
-    }
-
-    // pour loop Swipers
-    if ('loop' in this.element.dataset) {
-      this.options.loop = true;
-    }
-
-    // pour loop slides
-    if ('slides' in this.element.dataset) {
-      // console.log();
-      const chiffre = parseInt('slides', 10);
-      this.options.slidesPerView = 3.5 || this.options.slidesPerView;
+    // Pour vertical Swipers
+    if ('vertical' in this.element.dataset) {
+      this.options.direction = 'vertical';
     }
   }
 
